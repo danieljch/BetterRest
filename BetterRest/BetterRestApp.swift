e874ab90de54beb7b50569a7c75f1eaaa4a57a44
Daniel JCH <danielj.callisayah@gmail.com>
2021-12-14 16:06:33 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
